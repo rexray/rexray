485de5652246fdca85c55ce8417bf7d5c1b4b5c5
akutz <sakutz@gmail.com>
2015-08-07 17:43:01 -0500

    The rexraycli package renamed rexray
    The rexraycli package/directory has been renamed to rexray in order for
    the produced binary (the REX-ray CLI) to be named rexray.

---
+++ rexray/rexraycli.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/emccode/rexray/rexray/commands"
+
+func main() {
+	commands.Exec()
+}
--- rexraycli/rexraycli.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/emccode/rexraycli/commands"
-
-func main() {
-	commands.Exec()
-}

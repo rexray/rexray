2d8a60f15ab518acb9830bb62b50136bc75e6801
akutz <sakutz@gmail.com>
2015-10-19 17:11:14 -0500

    GoDoc Update to Root Package
    This patch updates the GoDoc comment on the root rexray package for
    visual niceness at godoc.org.

--- rexray.go
+++ rexray.go
@@ -1,3 +1,44 @@
+// Package rexray provides visibility and management of external/underlying
+// storage via guest storage introspection. Available as a Go package, CLI tool,
+// and Linux service, and with built-in third-party support for tools such as
+// Docker, REX-Ray is easily integrated into any workflow. For example, here's
+// how to list storage for a guest hosted on Amazon Web Services (AWS) with
+// REX-Ray:
+//
+//     [0]akutz@pax:~$ export REXRAY_STORAGEDRIVERS=ec2
+//     [0]akutz@pax:~$ export AWS_ACCESS_KEY=access_key
+//     [0]akutz@pax:~$ export AWS_SECRET_KEY=secret_key
+//     [0]akutz@pax:~$ rexray volume get
+//
+//     - providername: ec2
+//       instanceid: i-695bb6ab
+//       volumeid: vol-dedbadc3
+//       devicename: /dev/sda1
+//       region: us-west-1
+//       status: attached
+//     - providername: ec2
+//       instanceid: i-695bb6ab
+//       volumeid: vol-04c4b219
+//       devicename: /dev/xvdb
+//       region: us-west-1
+//       status: attached
+//
+//     [0]akutz@pax:~$
+//
+// Using REX-Ray as a library is easy too. To perform the same volume listing
+// as above, simply use the following snippet:
+//
+//     import "github.com/emccode/rexray"
+//
+//     r := rexray.NewWithEnv(map[string]string{
+//         "REXRAY_STORAGEDRIVERS": "ec2",
+//         "AWS_ACCESS_KEY": "access_key",
+//         "AWS_SECRET_KEY": "secret_key"})
+//
+//     r.InitDrivers()
+//
+//     volumes, err := r.Storage.GetVolumeMapping()
+//
 package rexray
 
 import (

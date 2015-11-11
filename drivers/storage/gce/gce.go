ec96b9b4bc5012e600965d3c3b30bfd615ed2d34
Lionel Touati <lionel@chictypes.com>
2015-11-11 20:12:54 +0100

    # This is a combination of 2 commits. # The first commit's message is:
    # This is a combination of 3 commits.
    # The first commit's message is:
    # This is a combination of 2 commits.
    # The first commit's message is:
    
    Implement a driver that supports Google Cloud Engine.
    
    # The 2nd commit message will be skipped:
    
    #	Another method ot read body
    
    # The 2nd commit message will be skipped:
    
    #	Fix Instance lookup
    
    # The 3rd commit message will be skipped:
    
    #	Fix Instance lookup
    
    # The 2nd commit message will be skipped:
    
    #	Readd those methods

--- drivers/storage/gce/gce.go
+++ drivers/storage/gce/gce.go
@@ -1,6 +1,7 @@
 package gce
 
 import (
+	"bytes"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/emccode/rexray/core"
@@ -10,21 +11,15 @@ import (
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/compute/v1"
 	"io/ioutil"
+	"net"
+	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const providerName = "gce"
-
-// The GCE storage driver.
-type driver struct {
-	client  *compute.Service
-	r       *core.RexRay
-	zone    string
-	project string
-}
-
 func ef() errors.Fields {
 	return errors.Fields{
 		"provider": providerName,
@@ -41,6 +36,15 @@ func eff(fields errors.Fields) map[string]interface{} {
 		}
 	}
 	return errFields
+}
+
+// The GCE storage driver.
+type driver struct {
+	currentInstanceId string
+	client            *compute.Service
+	r                 *core.RexRay
+	zone              string
+	project           string
 }
 
 func init() {
@@ -62,6 +66,7 @@ func (d *driver) Init(r *core.RexRay) error {
 	serviceAccountJSON, err := ioutil.ReadFile(d.r.Config.GetString("gce.keyfile"))
 	if err != nil {
 		log.WithField("provider", providerName).Fatalf("Could not read service account credentials file, %s => {%s}", d.r.Config.GetString("gce.keyfile"), err)
+		return err
 	}
 
 	config, err := google.JWTConfigFromJSON(serviceAccountJSON,
@@ -71,11 +76,41 @@ func (d *driver) Init(r *core.RexRay) error {
 
 	if err != nil {
 		log.WithField("provider", providerName).Fatalf("Could not create compute client => {%s}", err)
+		return err
 	}
 	d.client = client
+	instanceId, err := getCurrentInstanceId()
+	if err != nil {
+		log.WithField("provider", providerName).Fatalf("Could not get current  instance => {%s}", err)
+		return err
+	}
+	d.currentInstanceId = instanceId
 	log.WithField("provider", providerName).Info("storage driver initialized")
-
 	return nil
+}
+
+func getCurrentInstanceId() (string, error) {
+	conn, err := net.DialTimeout("tcp", "metadata.google.internal:80", 50 * time.Millisecond)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	url := "http://metadata.google.internal/computeMetadata/v1/instance/id"
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Metadata-Flavor", "Google")
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("Error: %v\n", err)
+	}
+
+	defer resp.Body.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	s := buf.String()
+	return s, nil
 }
 
 func (d *driver) Name() string {
@@ -84,17 +119,62 @@ func (d *driver) Name() string {
 
 func (d *driver) GetVolumeMapping() ([]*core.BlockDevice, error) {
 	log.WithField("provider", providerName).Debug("GetVolumeMapping")
-	return nil, nil
+
+	diskMap := make(map[string]*compute.Disk)
+	disks, err := d.client.Disks.List(d.project, d.zone).Do()
+	if err != nil {
+		return []*core.BlockDevice{}, err
+	}
+	for _, disk := range disks.Items {
+		diskMap[disk.SelfLink] = disk
+	}
+
+	instances, err := d.client.Instances.List(d.project, d.zone).Do()
+	if err != nil {
+		return []*core.BlockDevice{}, err
+	}
+	var ret []*core.BlockDevice
+	for _, instance := range instances.Items {
+		for _, disk := range instance.Disks {
+			ret = append(ret, &core.BlockDevice{
+				ProviderName: "gce",
+				InstanceID:   instance.Name,
+				VolumeID:     diskMap[disk.Source].Name,
+				DeviceName:   fmt.Sprintf("/dev/disk/by-id/google-%s", disk.DeviceName),
+				Region:       diskMap[disk.Source].Zone,
+				Status:       diskMap[disk.Source].Status,
+				NetworkName:  disk.Source,
+			})
+
+		}
+	}
+	return ret, nil
 }
 
 func (d *driver) GetInstance() (*core.Instance, error) {
 	log.WithField("provider", providerName).Debug("GetInstance")
+	query := d.client.Instances.List(d.project, d.zone)
+	instances, err := query.Do()
+	if err != nil {
+		return nil, err
+	}
+	for _, instance := range instances.Items {
+		if strconv.FormatUint(instance.Id, 10) == d.currentInstanceId {
+			return &core.Instance{
+				ProviderName: "gce",
+				InstanceID:   instance.Name,
+				Region:       instance.Zone,
+				Name:         instance.Name,
+			}, nil
+		}
+
+	}
 	return nil, nil
 }
 
 func (d *driver) CreateSnapshot(
-	runAsync bool,
-	snapshotName, volumeID, description string) ([]*core.Snapshot, error) {
+runAsync bool,
+snapshotName, volumeID, description string) ([]*core.Snapshot, error) {
 
 	log.WithField("provider", providerName).Debug("CreateSnapshot")
 	return nil, nil
@@ -102,7 +182,7 @@ func (d *driver) CreateSnapshot(
 }
 
 func (d *driver) GetSnapshot(
-	volumeID, snapshotID, snapshotName string) ([]*core.Snapshot, error) {
+volumeID, snapshotID, snapshotName string) ([]*core.Snapshot, error) {
 
 	log.WithField("provider", providerName).Debug("GetSnapshot")
 	return nil, nil
@@ -112,50 +192,6 @@ func (d *driver) RemoveSnapshot(snapshotID string) error {
 	log.WithField("provider", providerName).Debug("RemoveSnapshot")
 	return nil
 }
-
-func (d *driver) GetDeviceNextAvailable() (string, error) {
-	letters := []string{
-		"a", "b", "c", "d", "e", "f", "g", "h",
-		"i", "j", "k", "l", "m", "n", "o", "p"}
-
-	blockDeviceNames := make(map[string]bool)
-
-	blockDeviceMapping, err := d.GetVolumeMapping()
-	if err != nil {
-		return "", err
-	}
-
-	for _, blockDevice := range blockDeviceMapping {
-		re, _ := regexp.Compile(`^/dev/xvd([a-z])`)
-		res := re.FindStringSubmatch(blockDevice.DeviceName)
-		if len(res) > 0 {
-			blockDeviceNames[res[1]] = true
-		}
-	}
-
-	localDevices, err := getLocalDevices()
-	if err != nil {
-		return "", err
-	}
-
-	for _, localDevice := range localDevices {
-		re, _ := regexp.Compile(`^xvd([a-z])`)
-		res := re.FindStringSubmatch(localDevice)
-		if len(res) > 0 {
-			blockDeviceNames[res[1]] = true
-		}
-	}
-
-	for _, letter := range letters {
-		if !blockDeviceNames[letter] {
-			nextDeviceName := "/dev/xvd" + letter
-			log.Println("Got next device name: " + nextDeviceName)
-			return nextDeviceName, nil
-		}
-	}
-	return "", errors.New("No available device")
-}
-
 func getLocalDevices() (deviceNames []string, err error) {
 	file := "/proc/partitions"
 	contentBytes, err := ioutil.ReadFile(file)
@@ -176,47 +212,145 @@ func getLocalDevices() (deviceNames []string, err error) {
 	return deviceNames, nil
 }
 
+func (d *driver) GetDeviceNextAvailable() (string, error) {
+	letters := []string{
+		"a", "b", "c", "d", "e", "f", "g", "h",
+		"i", "j", "k", "l", "m", "n", "o", "p"}
+
+	blockDeviceNames := make(map[string]bool)
+
+	blockDeviceMapping, err := d.GetVolumeMapping()
+	if err != nil {
+		return "", err
+	}
+
+	for _, blockDevice := range blockDeviceMapping {
+		re, _ := regexp.Compile(`^/dev/sd([a-z])`)
+		res := re.FindStringSubmatch(blockDevice.DeviceName)
+		if len(res) > 0 {
+			blockDeviceNames[res[1]] = true
+		}
+	}
+
+	localDevices, err := getLocalDevices()
+	if err != nil {
+		return "", err
+	}
+
+	for _, localDevice := range localDevices {
+		re, _ := regexp.Compile(`^sd([a-z])`)
+		res := re.FindStringSubmatch(localDevice)
+		if len(res) > 0 {
+			blockDeviceNames[res[1]] = true
+		}
+	}
+
+	for _, letter := range letters {
+		if !blockDeviceNames[letter] {
+			nextDeviceName := "/dev/sd" + letter
+			log.Println("Got next device name: " + nextDeviceName)
+			return nextDeviceName, nil
+		}
+	}
+	return "", errors.New("No available device")
+}
+func (d *driver) waitUntilOperationIsFinished(operation *compute.Operation) error {
+	opName := operation.Name
+	OpLoop:
+	for {
+		time.Sleep(100 * time.Millisecond)
+		op, err := d.client.ZoneOperations.Get(d.project, d.zone, opName).Do()
+		if err != nil {
+			return err
+		}
+		switch op.Status {
+		case "PENDING", "RUNNING":
+			continue
+		case "DONE":
+			if op.Error != nil {
+				return err
+			}
+			break OpLoop
+		default:
+			log.WithField("provider", providerName).Fatalf("Unknown status %q: %+v", op.Status, op)
+			return nil
+		}
+	}
+	return nil
+}
 func (d *driver) CreateVolume(
-	runAsync bool, volumeName, volumeID, snapshotID, volumeType string,
-	IOPS, size int64, availabilityZone string) (*core.Volume, error) {
-	log.WithField("provider", providerName).Debug("CreateVolume")
-	return nil, nil
+runAsync bool, volumeName, volumeID, snapshotID, volumeType string,
+IOPS, size int64, availabilityZone string) (*core.Volume, error) {
+	log.WithFields(log.Fields{
+		"provider":         providerName,
+		"volumeName":       volumeName,
+		"volumeID":         volumeID,
+		"snapshotID":       snapshotID,
+		"volumeType":       volumeType,
+		"availabilityZone": availabilityZone}).Debug("CreateVolume")
 
+	if availabilityZone == "" {
+		availabilityZone = d.zone
+	}
+	disk := &compute.Disk{
+		Name:   volumeName,
+		Zone:   availabilityZone,
+		Type:   "https://www.googleapis.com/compute/v1/projects/gce-dev-1060/zones/europe-west1-b/diskTypes/pd-standard",
+		SizeGb: size,
+	}
+	createdVolume, err := d.client.Disks.Insert(d.project, d.zone, disk).Do()
+	if err != nil {
+		return nil, err
+	}
+	if !runAsync {
+		err := d.waitUntilOperationIsFinished(createdVolume)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+	volume, err := d.GetVolume(volumeName, "")
+	if err != nil {
+		return nil, err
+	}
+	return volume[0], nil
 }
 
-func (d *driver) createVolumeCreateSnapshot(
-	volumeID string, snapshotID string) (string, error) {
-	log.WithField("provider", providerName).Debug("CreateVolumeCreateSnapshot")
-	return "", nil
+func (d *driver) getVolumesAttachedToInstance(instances []*compute.Instance,volumeID string) []*core.VolumeAttachment {
+	var attachments []*core.VolumeAttachment
+	for _, instance := range instances {
+		for _, disk := range instance.Disks {
+			shortVolume := strings.Replace(disk.Source,
+				fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/zones/%s/disks/", d.project, d.zone),
+				"",
+				-1)
 
+			if volumeID !="" && shortVolume!=volumeID {
+				continue
+			}
+			attachments = append(attachments, d.convertGCEAttachedDisk(instance.Name, disk))
+		}
+	}
+
+	return attachments
 }
-
 func (d *driver) GetVolume(
-	volumeID, volumeName string) ([]*core.Volume, error) {
+volumeID, volumeName string) ([]*core.Volume, error) {
 	log.WithField("provider", providerName).Debugf("GetVolume :%s %s", volumeID, volumeName)
+
+	instances, err := d.client.Instances.List(d.project, d.zone).Do()
+	if err != nil {
+		return []*core.Volume{}, err
+	}
+	attachments := d.getVolumesAttachedToInstance(instances.Items,"")
 
 	query := d.client.Disks.List(d.project, d.zone)
 	if volumeID != "" {
-		query.Filter(fmt.Sprintf("id eq %s", volumeID))
+		query.Filter(fmt.Sprintf("name eq %s", volumeID))
 	}
 	if volumeName != "" {
 		query.Filter(fmt.Sprintf("name eq %s", volumeName))
 	}
-	var attachments []*core.VolumeAttachment
-	instances, err := d.client.Instances.List(d.project, d.zone).Do()
-	for _, instance := range instances.Items {
-		for _, disk := range instance.Disks {
-			attachment := &core.VolumeAttachment{
-				InstanceID: strconv.FormatUint(instance.Id, 10),
-				DeviceName: disk.DeviceName,
-				Status:     disk.Mode,
-				VolumeID:   disk.Source,
-			}
-			attachments = append(attachments, attachment)
-
-		}
-	}
-
 	disks, err := query.Do()
 	if err != nil {
 		return []*core.Volume{}, err
@@ -228,7 +362,7 @@ func (d *driver) GetVolume(
 			if attachment.VolumeID == disk.SelfLink {
 				diskAttachments = append(diskAttachments, &core.VolumeAttachment{
 					InstanceID: attachment.InstanceID,
-					DeviceName: attachment.DeviceName,
+					DeviceName: fmt.Sprintf("/dev/disk/by-id/google-%s", attachment.DeviceName),
 					Status:     attachment.Status,
 					VolumeID:   strconv.FormatUint(disk.Id, 10),
 				})
@@ -236,7 +370,7 @@ func (d *driver) GetVolume(
 		}
 		volumeSD := &core.Volume{
 			Name:             disk.Name,
-			VolumeID:         strconv.FormatUint(disk.Id, 10),
+			VolumeID:         disk.Name,
 			AvailabilityZone: disk.Zone,
 			Status:           disk.Status,
 			VolumeType:       disk.Kind,
@@ -251,50 +385,122 @@ func (d *driver) GetVolume(
 	return volumesSD, nil
 }
 
+func (d *driver) convertGCEAttachedDisk(instanceName string, disk *compute.AttachedDisk) *core.VolumeAttachment {
+	return &core.VolumeAttachment{
+		InstanceID: instanceName,
+		DeviceName: fmt.Sprintf("/dev/disk/by-id/google-%s", disk.DeviceName),
+		Status:     disk.Mode,
+		VolumeID:   disk.Source,
+	}
+}
+
 func (d *driver) GetVolumeAttach(
-	volumeID, instanceID string) ([]*core.VolumeAttachment, error) {
-	log.WithField("provider", providerName).Debug("GetVolumeAttach")
-	return nil, nil
-}
-
-func (d *driver) waitSnapshotComplete(snapshotID string) error {
-	return nil
-}
-
-func (d *driver) waitVolumeComplete(volumeID string) error {
-	return nil
-}
-
-func (d *driver) waitVolumeAttach(volumeID, instanceID string) error {
-	return nil
-}
-
-func (d *driver) waitVolumeDetach(volumeID string) error {
-	return nil
+volumeID, instanceID string) ([]*core.VolumeAttachment, error) {
+	log.WithField("provider", providerName).Debugf("GetVolumeAttach :%s %s", volumeID, instanceID)
+	query := d.client.Instances.List(d.project, d.zone)
+	if instanceID != "" {
+		query.Filter(fmt.Sprintf("name eq %s", instanceID))
+	}
+	instances, err := query.Do()
+	if err != nil {
+		return []*core.VolumeAttachment{}, err
+	}
+	return d.getVolumesAttachedToInstance(instances.Items,volumeID),nil;
 }
 
 func (d *driver) RemoveVolume(volumeID string) error {
-	return nil
+	log.WithField("provider", providerName).Debugf("RemoveVolume :%s", volumeID)
+	_, err := d.client.Disks.Delete(d.project, d.zone, volumeID).Do()
+	return err
+
 }
 
 func (d *driver) AttachVolume(
-	runAsync bool,
-	volumeID, instanceID string) ([]*core.VolumeAttachment, error) {
-	log.WithField("provider", providerName).Debug("AttachVolume")
-	return nil, nil
+runAsync bool,
+volumeID, instanceID string) ([]*core.VolumeAttachment, error) {
+	if instanceID == "" {
+		instanceID = d.currentInstanceId
+	}
+	instance, err := d.GetInstance()
+	if err != nil {
+		return nil, err
+	}
+	instanceID = instance.Name
+	log.WithField("provider", providerName).Debugf("AttachVolume %s %s", volumeID, instance.Name)
+	query := d.client.Disks.List(d.project, d.zone)
+	query.Filter(fmt.Sprintf("name eq %s", volumeID))
+	disks, err := query.Do()
+	if err != nil {
+		return nil, err
+	}
+	if len(disks.Items) != 1 {
+		return nil, errors.New("No available device")
+	}
+
+	disk := &compute.AttachedDisk{
+		AutoDelete: false,
+		Boot:       false,
+		Source:     disks.Items[0].SelfLink,
+	}
+	operation, err := d.client.Instances.AttachDisk(d.project, d.zone, instanceID, disk).Do()
+	if err != nil {
+		return nil, err
+	}
+	if !runAsync {
+		err := d.waitUntilOperationIsFinished(operation)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return d.GetVolumeAttach(volumeID, instanceID)
 
 }
 
 func (d *driver) DetachVolume(
-	runAsync bool,
-	volumeID, blank string) error {
-	log.WithField("provider", providerName).Debug("DetachVolume")
+runAsync bool,
+volumeID, blank string) error {
+	instance, err := d.GetInstance()
+	if err != nil {
+		return err
+	}
+	instanceID := instance.Name
+	log.WithField("provider", providerName).Debugf("DetachVolume %s %s", volumeID, instance.Name)
+	query := d.client.Disks.List(d.project, d.zone)
+	query.Filter(fmt.Sprintf("name eq %s", volumeID))
+	disks, err := query.Do()
+	if err != nil {
+		return err
+	}
+	if len(disks.Items) != 1 {
+		return errors.New("No available device")
+	}
+	attachements, err := d.GetVolumeAttach(volumeID, instanceID)
+	for _, attachement := range attachements {
+		targetVolumeId := strings.Replace(attachement.VolumeID,
+			fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/zones/%s/disks/", d.project, d.zone),
+			"",
+			-1)
+		if (targetVolumeId == volumeID) {
+			diskName := strings.Replace(attachement.DeviceName, "/dev/disk/by-id/google-", "", -1)
+			operation, err := d.client.Instances.DetachDisk(d.project, d.zone, instanceID, diskName).Do()
+			if err != nil {
+				return err
+			}
+			if !runAsync {
+				err := d.waitUntilOperationIsFinished(operation)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 	return nil
 }
 
 func (d *driver) CopySnapshot(runAsync bool,
-	volumeID, snapshotID, snapshotName, destinationSnapshotName,
-	destinationRegion string) (*core.Snapshot, error) {
+volumeID, snapshotID, snapshotName, destinationSnapshotName,
+destinationRegion string) (*core.Snapshot, error) {
 	log.WithField("provider", providerName).Debug("CopySnapshot")
 	return nil, nil
 }

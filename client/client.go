c133c5d6a112a1998ad99a2a520e2bcff49eec7a
akutz <sakutz@gmail.com>
2016-04-21 10:42:52 -0500

    Refactored Client Interfaces & Enhanced Tests
    This patch refactors the Client interfaces such that ./api/client now
    has the Client interface which is the base for the ./client/Client and
    ./api/client/APIClient interfaces. The ./api/client/APIClient interface
    is what the ./api/client/Client interface used to be. The
    ./client/Client interface is a superset of the ./api/client/Client
    functions with the addition of helper functions. However, the
    ./client/Client object returned by the NewClient function can be cast to
    ./api/client/APIClient when it's necessary to access underlying,
    advanced API functions.
    
    Finally the test harness has been enhanced to provided canned test
    routines for testing executors, getting the instance ID, and validating
    common API routes.

--- client/client.go
+++ client/client.go
@@ -7,8 +7,6 @@ import (
 	"github.com/akutz/gotil"
 
 	apiclient "github.com/emccode/libstorage/api/client"
-	"github.com/emccode/libstorage/api/types"
-	apihttp "github.com/emccode/libstorage/api/types/http"
 
 	// load the drivers
 	_ "github.com/emccode/libstorage/drivers/os"
@@ -20,32 +18,12 @@ var (
 
 // Client is the libStorage client.
 type Client interface {
-
-	// Services returns a map of the configured Services.
-	Services() (apihttp.ServicesMap, error)
-
-	// ServiceInspect returns information about a service.
-	ServiceInspect(name string) (*types.ServiceInfo, error)
-
-	// Volumes returns all volumes for all configured services.
-	Volumes(attachments bool) (apihttp.ServiceVolumeMap, error)
-
-	// VolumeInspect gets information about a single volume.
-	VolumeInspect(
-		service, volumeID string, attachments bool) (*types.Volume, error)
-
-	// VolumeCreate creates a single volume.
-	VolumeCreate(
-		service string, request *apihttp.VolumeCreateRequest) (*types.Volume, error)
-
-	// VolumeRemove removes a single volume.
-	VolumeRemove(
-		service, volumeID string) error
+	apiclient.Client
 }
 
 type client struct {
+	apiclient.APIClient
 	config gofig.Config
-	apicli apiclient.Client
 }
 
 // New returns a new Client.
@@ -57,39 +35,11 @@ func New(config gofig.Config) (Client, error) {
 			config = cfg
 		}
 	}
-	apicli, err := apiclient.Dial(nil, config)
+	ac, err := apiclient.Dial(nil, config)
 	if err != nil {
 		return nil, err
 	}
-	return &client{config: config, apicli: apicli}, nil
-}
-
-func (c *client) Services() (apihttp.ServicesMap, error) {
-	return c.apicli.Services()
-}
-
-func (c *client) ServiceInspect(service string) (*types.ServiceInfo, error) {
-	return c.apicli.ServiceInspect(service)
-}
-
-func (c *client) Volumes(
-	attachments bool) (apihttp.ServiceVolumeMap, error) {
-	return c.apicli.Volumes()
-}
-
-func (c *client) VolumeInspect(
-	service, volumeID string, attachments bool) (*types.Volume, error) {
-	return c.apicli.VolumeInspect(service, volumeID, attachments)
-}
-
-func (c *client) VolumeCreate(
-	service string, request *apihttp.VolumeCreateRequest) (*types.Volume, error) {
-	return c.apicli.VolumeCreate(service, request)
-}
-
-func (c *client) VolumeRemove(
-	service, volumeID string) error {
-	return c.apicli.VolumeRemove(service, volumeID)
+	return &client{APIClient: ac, config: config}, nil
 }
 
 func getNewConfig() (gofig.Config, error) {

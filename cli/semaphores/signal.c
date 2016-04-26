ed824f0738394e04a23a77867ac5b7ad1dc4a79c
akutz <sakutz@gmail.com>
2016-04-26 11:25:35 -0500

    Semaphore Tools
    This patch adds some tools to help manage semaphores during development.
    The tools are compiled to ./cli/semaphores:
    
        * open    - opens a named semaphore with an initial value of 1
        * wait    - blocks until the named semaphore has a value > 0
        * signal  - increments the named semaphore's value by 1
        * unlink  - unlinks the named semaphore

---
+++ cli/semaphores/signal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <semaphore.h>
+#include <errno.h>
+
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		printf("signal: %s <semaphore_name>\n", argv[0]);
+		return 1;
+	}
+	sem_t* sem = sem_open(argv[1], O_CREAT, 0644, 1);
+	if (sem == SEM_FAILED) {
+		return errno;
+	}
+	return sem_post(sem) ? errno : 0;
+}

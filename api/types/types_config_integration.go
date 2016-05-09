065cc5a198f535500f85f5554f25efed133eb50c
Clinton Kitson <clintonskitson@gmail.com>
2016-05-09 16:34:05 -0700

    Updated volume config keys to be in integration package
    All of the configurations for volumes are not int the integration
    package. The assumption is that new drivers would also use the
    same keys since they could be global.
    
    
    Former-commit-id: 9f33312129145631827c99e83548dc3ff7563751

--- api/types/types_config_integration.go
+++ api/types/types_config_integration.go
@@ -1,24 +1,69 @@
 package types
 
 const (
-	//ConfigIntegration is a config key.
-	ConfigIntegration = ConfigRoot + ".integration"
+	//ConfigIg is a config key.
+	ConfigIg = ConfigRoot + ".integration"
 
-	// ConfigIntegrationVolMountPreempt is a config key.
-	ConfigIntegrationVolMountPreempt = ConfigIntegration + ".volume.mount.preempt"
+	//ConfigIgVol is a config key.
+	ConfigIgVol = ConfigIg + ".volume"
 
-	// ConfigIntegrationVolCreateDisable is a config key.
-	ConfigIntegrationVolCreateDisable = ConfigIntegration + ".volume.create.disable"
+	//ConfigIgVolOps is a config key.
+	ConfigIgVolOps = ConfigIgVol + ".operations"
 
-	// ConfigIntegrationVolRemoveDisable is a config key.
-	ConfigIntegrationVolRemoveDisable = ConfigIntegration + ".volume.remove.disable"
+	//ConfigIgVolOpsMount is a config key.
+	ConfigIgVolOpsMount = ConfigIgVolOps + ".mount"
 
-	// ConfigIntegrationVolUnmountIgnoreUsed is a config key.
-	ConfigIntegrationVolUnmountIgnoreUsed = ConfigIntegration + ".volume.unmount.ignoreusedcount"
+	//ConfigIgVolOpsMountPreempt is a config key.
+	ConfigIgVolOpsMountPreempt = ConfigIgVolOpsMount + ".preempt"
 
-	// ConfigIntegrationVolPathCache is a config key.
-	ConfigIntegrationVolPathCache = ConfigIntegration + ".volume.path.cache"
+	//ConfigIgVolOpsMountPath is a config key.
+	ConfigIgVolOpsMountPath = ConfigIgVolOpsMount + ".path"
 
-	//ConfigIntegrationVolCreateImplicit is a config key.
-	ConfigIntegrationVolCreateImplicit = ConfigIntegration + ".volume.create.implicit"
+	//ConfigIgVolOpsMountRootPath is a config key.
+	ConfigIgVolOpsMountRootPath = ConfigIgVolOpsMount + ".rootPath"
+
+	//ConfigIgVolOpsUnmount is a config key.
+	ConfigIgVolOpsUnmount = ConfigIgVolOps + ".unmount"
+
+	//ConfigIgVolOpsUnmountIgnoreUsed is a config key.
+	ConfigIgVolOpsUnmountIgnoreUsed = ConfigIgVolOpsUnmount + ".ignoreusedcount"
+
+	// ConfigIgVolOpsPath is a config key.
+	ConfigIgVolOpsPath = ConfigIgVolOps + ".path"
+
+	// ConfigIgVolOpsPathCache is a config key.
+	ConfigIgVolOpsPathCache = ConfigIgVolOpsPath + ".cache"
+
+	// ConfigIgVolOpsCreate is a config key.
+	ConfigIgVolOpsCreate = ConfigIgVolOps + ".create"
+
+	// ConfigIgVolOpsCreateDisable is a config key.
+	ConfigIgVolOpsCreateDisable = ConfigIgVolOpsCreate + ".disable"
+
+	// ConfigIgVolOpsCreateImplicit is a config key.
+	ConfigIgVolOpsCreateImplicit = ConfigIgVolOpsCreate + ".implicit"
+
+	// ConfigIgVolOpsCreateDefault is a config key.
+	ConfigIgVolOpsCreateDefault = ConfigIgVolOpsCreate + ".default"
+
+	// ConfigIgVolOpsCreateDefaultSize is a config key.
+	ConfigIgVolOpsCreateDefaultSize = ConfigIgVolOpsCreateDefault + ".size"
+
+	// ConfigIgVolOpsCreateDefaultFsType is a config key.
+	ConfigIgVolOpsCreateDefaultFsType = ConfigIgVolOpsCreateDefault + ".fsType"
+
+	// ConfigIgVolOpsCreateDefaultAZ is a config key.
+	ConfigIgVolOpsCreateDefaultAZ = ConfigIgVolOpsCreateDefault + ".availabilityZone"
+
+	// ConfigIgVolOpsCreateDefaultType is a config key.
+	ConfigIgVolOpsCreateDefaultType = ConfigIgVolOpsCreateDefault + ".type"
+
+	// ConfigIgVolOpsCreateDefaultIOPS is a config key.
+	ConfigIgVolOpsCreateDefaultIOPS = ConfigIgVolOpsCreateDefault + ".IOPS"
+
+	// ConfigIgVolOpsRemove is a config key.
+	ConfigIgVolOpsRemove = ConfigIgVolOps + ".remove"
+
+	// ConfigIgVolOpsRemoveDisable is a config key.
+	ConfigIgVolOpsRemoveDisable = ConfigIgVolOpsRemove + ".disable"
 )

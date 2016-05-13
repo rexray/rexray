ec16928f5c10d6a2450eabf5e5819bc24b416c07
akutz <sakutz@gmail.com>
2016-05-13 18:38:18 -0500

    Removed VirtualBox VolumePath Default Value
    The VirtualBox VolumePath default value has been removed as it was
    invalid.

--- drivers/storage/vbox/vbox.go
+++ drivers/storage/vbox/vbox.go
@@ -1,11 +1,6 @@
 package vbox
 
-import (
-	"github.com/akutz/gofig"
-	"github.com/akutz/gotil"
-
-	"github.com/emccode/libstorage/api/utils/paths"
-)
+import "github.com/akutz/gofig"
 
 const (
 	// Name is the provider's name.
@@ -18,13 +13,11 @@ func init() {
 
 func registerConfig() {
 
-	volumePath := paths.Join(gotil.HomeDir(), "VirtualBox", "Volumes")
-
 	r := gofig.NewRegistration("Oracle VM VirtualBox")
 	r.Key(gofig.String, "", "", "", "virtualbox.username")
 	r.Key(gofig.String, "", "", "", "virtualbox.password")
 	r.Key(gofig.String, "", "http://127.0.0.1:18083", "", "virtualbox.endpoint")
-	r.Key(gofig.String, "", volumePath, "", "virtualbox.volumePath")
+	r.Key(gofig.String, "", "", "", "virtualbox.volumePath")
 	r.Key(gofig.String, "", "", "", "virtualbox.localMachineNameOrId")
 	r.Key(gofig.Bool, "", false, "", "virtualbox.tls")
 	r.Key(gofig.String, "", "SATA", "", "virtualbox.controllerName")

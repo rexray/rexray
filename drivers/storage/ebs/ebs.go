bde0277315348d83a6a1f5d2b07604f02f1d542c
akutz <sakutz@gmail.com>
2016-10-01 11:45:31 -0500

    EBS Session Caching
    This patch introduces EBS session caching. Now that EBS sessions are
    scoped to HTTP requests and shuttled around with the Context object, it
    becomes necessary to optimize their usage. Session objects are now
    cached using a key that is the MD5 checksum of the concatenation of the
    following string values that are identifying characteristics of an EBS
    session: region, endpoint, and access key. Any unique combination of
    those values (including if one is absent) will produce a unique session
    object that is then cached for future use, available to requests with
    the same identifying characteristics for establishing an EBS session.

--- drivers/storage/ebs/ebs.go
+++ drivers/storage/ebs/ebs.go
@@ -24,6 +24,24 @@ const (
 	// InstanceIDFieldAvailabilityZone is the key to retrieve the availability
 	// zone value from the InstanceID Field map.
 	InstanceIDFieldAvailabilityZone = "availabilityZone"
+
+	// AccessKey is a key constant.
+	AccessKey = "accessKey"
+
+	// SecretKey is a key constant.
+	SecretKey = "secretKey"
+
+	// Region is a key constant.
+	Region = "region"
+
+	// Endpoint is a key constant.
+	Endpoint = "endpoint"
+
+	// MaxRetries is a key constant.
+	MaxRetries = "maxRetries"
+
+	// Tag is a key constant.
+	Tag = "tag"
 )
 
 func init() {
@@ -32,17 +50,17 @@ func init() {
 
 func registerConfig() {
 	r := gofig.NewRegistration("EBS")
-	r.Key(gofig.String, "", "", "", "ebs.accessKey")
-	r.Key(gofig.String, "", "", "", "ebs.secretKey")
-	r.Key(gofig.String, "", "", "", "ebs.region")
-	r.Key(gofig.String, "", "", "", "ebs.endpoint")
-	r.Key(gofig.Int, "", DefaultMaxRetries, "", "ebs.maxRetries")
-	r.Key(gofig.String, "", "", "Tag prefix for EBS naming", "ebs.tag")
-	r.Key(gofig.String, "", "", "", "ec2.accessKey")
-	r.Key(gofig.String, "", "", "", "ec2.secretKey")
-	r.Key(gofig.String, "", "", "", "ec2.region")
-	r.Key(gofig.String, "", "", "", "ec2.endpoint")
-	r.Key(gofig.Int, "", DefaultMaxRetries, "", "ec2.maxRetries")
-	r.Key(gofig.String, "", "", "Tag prefix for EBS naming", "ec2.tag")
+	r.Key(gofig.String, "", "", "", Name+"."+AccessKey)
+	r.Key(gofig.String, "", "", "", Name+"."+SecretKey)
+	r.Key(gofig.String, "", "", "", Name+"."+Region)
+	r.Key(gofig.String, "", "", "", Name+"."+Endpoint)
+	r.Key(gofig.Int, "", DefaultMaxRetries, "", Name+"."+MaxRetries)
+	r.Key(gofig.String, "", "", "Tag prefix for EBS naming", Name+"."+Tag)
+	r.Key(gofig.String, "", "", "", OldName+"."+AccessKey)
+	r.Key(gofig.String, "", "", "", OldName+"."+SecretKey)
+	r.Key(gofig.String, "", "", "", OldName+"."+Region)
+	r.Key(gofig.String, "", "", "", OldName+"."+Endpoint)
+	r.Key(gofig.Int, "", DefaultMaxRetries, "", OldName+"."+MaxRetries)
+	r.Key(gofig.String, "", "", "Tag prefix for EBS naming", OldName+"."+Tag)
 	gofig.Register(r)
 }

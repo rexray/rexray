0224c4f91d7d6a1bd01e94b1a5d843026854e1a3
akutz <sakutz@gmail.com>
2016-10-08 03:32:12 -0500

    EBS Vagrant Updates
    This patch contains updates to the EBS test Vagrantfile work.

--- drivers/storage/ebs/tests/server-tests.sh
+++ drivers/storage/ebs/tests/server-tests.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# create new docker volumes named db1, db2, and db3
-docker volume create --driver rexray --name db1
-docker volume create --driver rexray --name db2
-docker volume create --driver rexray --name db3
+# create new docker volumes named builds
+docker volume create --driver rexray --name builds
